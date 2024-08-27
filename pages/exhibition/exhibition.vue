2327f956de2f0888d99235ca12f4eb5f354fc7da
FengduXS <2418869820@qq.com>
2024-08-27 23:59:31 +0800

    sds

--- pages/exhibition/exhibition.vue
+++ pages/exhibition/exhibition.vue
@@ -1,34 +1,20 @@
 <template>
   <view class="container">
     <!-- <web-view src="http://117.50.174.221/"></web-view> -->
-    <web-view src="https://cdc.crowninfo.cn#wechat_redirect"></web-view>
-  <!-- <cover-view class="share-button">
-      <cover-view class="exit" @click="exitExhibition">
-        退出
-      </cover-view>
-     <cover-view class="check" @click="checkCredits">
-       查看积分
-      </cover-view>
-    </cover-view> -->
+    <web-view src="https://cdc.crowninfo.cn"></web-view>
   </view>
 </template>
 
 <script>
 export default {
-  methods: {
-    exitExhibition() {
-      wx.navigateBack({
-        delta: 1 
-      });
-    },
-    checkCredits() {
-      wx.showModal({
-        title: '积分信息',
-        content: '您的积分为: XXXX',
-        showCancel: false
-      });
-    }
-  }
+	data(){
+		return {
+			url:'https://cdc.crowninfo.cn'
+		}
+	},
+	onLoad() {
+		this.url = encodeURI(this.url)
+	},
 }
 </script>
 
@@ -37,20 +23,5 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
-}
-
-.share-button {
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
-  z-index: 10;
-}
-
-.exit, .check {
-  display: inline-block;
-  margin-left: 10px;
-  padding: 5px 10px;
-  border: 1px solid #000;
-  border-radius: 5px;
 }
 </style>
